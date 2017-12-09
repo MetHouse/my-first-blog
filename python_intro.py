1485cd8f86a2e1dd8ae4f7c1eb19a9c17ec79375
MetHouse <angelkisa1996@mail.ru>
2017-12-09 14:58:50 +0300

    My Django Girls app, first commit

---
+++ python_intro.py
@@ -0,0 +1,29 @@
+#import win_unicode_console
+#win_unicode_console.enable()
+##if 3 > 2:
+#      print ("It works!")
+#if 5 > 2:
+#   print("5 is indeed greather than 2")      
+#else:
+#    print("5 is not greather than 2")
+#name = "Sonja"
+#if name == "Ola":
+#    print("hey Ola!")
+#elif name == "Sonja!"
+#    print("hey Sonja!")
+#else:
+#   print("Hey anonymous!")
+#    if  name == 'Ola':
+ #       print('Hi Ola!')
+#    elif name == 'Sonja':
+#        print('Hi Sonja!')
+#   else:
+#hi("Sonja")
+def hi(name):
+    print("Hi "+name+"!")
+girls = ['Rachel','Monica',"Phoebe","Ola","You"]
+for name in girls:
+    hi(name)
+    print("next girls")
+for i in range(1,6):
+        print(i)
